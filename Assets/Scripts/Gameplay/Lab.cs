ca77bc803c1fd5b76b8d457ea9c7c9879328655c
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2026-08-22 21:11:52 +0900

    누락된 커맨드 아이콘들 추가

--- Assets/Scripts/Gameplay/Lab.cs
+++ Assets/Scripts/Gameplay/Lab.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,8 @@ public class Lab : MonoBehaviour, ICommandable, ISavable, IBuildable
 {
     [SerializeField] private WorkbenchUI _workbenchUiPrefab;
     [SerializeField] private Sprite _workbenchButtonIcon;
+    // 철거 명령 버튼에 표시할 아이콘
+    [SerializeField] private Sprite _demolishButtonIcon;
     // 저장/복원 기능을 제공하는 컴포지션. PrefabId는 인스펙터에서 설정한다.
     [SerializeField] private SavableHandler _savable = new();
 
@@ -43,7 +45,7 @@ public class Lab : MonoBehaviour, ICommandable, ISavable, IBuildable
         _commands = new List<CommandData>
         {
             new CommandData("작업대 열기", _workbenchButtonIcon, OpenWorkbenchUI),
-            new CommandData("철거", null, ExecuteDemolish),
+            new CommandData("철거", _demolishButtonIcon, ExecuteDemolish),
         };
     }
 
